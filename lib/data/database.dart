65ddf77d9a9ec5ad46f606b2364da28eac2225bb
dhanushgit72 <dhanushdouda711@gmail.com>
2024-03-13 18:21:19 +0530

    first commit

---
+++ lib/data/database.dart
@@ -0,0 +1,15 @@
+
+import 'package:hive_flutter/hive_flutter.dart';
+class ToDoDataBase{
+  List toDoList=[];
+  final _myBox=Hive.box("mybox");
+  void createInitialData(){
+    toDoList=[["Welcome",false]];
+  }
+  void loadData(){
+    toDoList=_myBox.get("TODOLIST");
+  }
+  void updateDatabase(){
+    _myBox.put("TODOLIST",toDoList);
+  }
+}
